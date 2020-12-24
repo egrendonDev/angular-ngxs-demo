b27d5c025d48786b85bf29025f508642b55c0a8a
Ernesto Rendon <egrendon.dev@gmail.com>
2020-12-24 08:17:41 -0700

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoursesListComponent } from './components/course/courses-list/courses-list.component';
 import { CreateCourseComponent } from './components/course/create-course/create-course.component';
+import { UserEditComponent } from './components/user/user-edit/user-edit.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
 
 const routes = [
-  {
-    path: 'courses',
-    component: CoursesListComponent
-  },
+  { path: 'courses', component: CoursesListComponent},
   {path: 'create-course', component: CreateCourseComponent},
+  {path: 'users', component: UserListComponent},
+  {path: 'create-user', component: UserEditComponent},
   {path: '**', redirectTo: 'courses'}
 ];
 
